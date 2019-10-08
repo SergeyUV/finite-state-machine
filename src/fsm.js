430b6a31a2986324fa3568c40ef7ae89862735df
Sergey Vnukov <skivun@hotmail.com>
2019-10-08 14:43:59 +0300

    task: complete all methods except undo() and redo()

--- src/fsm.js
+++ src/fsm.js
@@ -1,32 +1,103 @@
+//------- tmp 
+// const config = {
+//     initial: 'normal',
+//     states: {
+//         normal: {
+//             transitions: {
+//                 study: 'busy',
+//             }
+//         },
+//         busy: {
+//             transitions: {
+//                 get_tired: 'sleeping',
+//                 get_hungry: 'hungry',
+//             }
+//         },
+//         hungry: {
+//             transitions: {
+//                 eat: 'normal'
+//             },
+//         },
+//         sleeping: {
+//             transitions: {
+//                 get_hungry: 'hungry',
+//                 get_up: 'normal',
+//             },
+//         },
+//     }
+// };
+//-----
+
+
 class FSM {
     /**
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+
+        if (! config){
+            throw new Error('Undefined FSM Config ');
+        }
+
+        this.state = config.initial;
+        this.config = config;
+        this.history = [];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+
+        return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        
+        let stateFound = false;
+        for (let st in this.config.states){
+            if(st == state){
+                stateFound = true;
+                break;
+            }            
+        }
+        
+        if(! stateFound) throw new Error( `State '${state}' not found`);
+
+        this.state = state;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        
+        let evenForStateFound = false;
+        for(let ev in this.config.states[this.state].transitions){
+
+            if(ev == event){
+                evenForStateFound = true;
+                this.state = this.config.states[this.state].transitions[ev];
+                break;
+            }
+        }
+
+        if (!evenForStateFound) throw new Error(`Event ${event} not found for current state`);
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.state = this.config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,7 +105,26 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        
+        let retVal = [];
+        
+        for (let st in this.config.states){
+            if( !event ){
+                retVal.push(st);
+                continue;
+            }
+            
+            for(let ev in this.config.states[st].transitions){
+
+                if(ev == event){
+                    retVal.push(st);    
+                }
+            }
+        }
+        
+        return retVal;
+    }
 
     /**
      * Goes back to previous state.
@@ -53,8 +143,18 @@ class FSM {
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.history.length = 0;
+    }
 }
+
+ //const student = new FSM(config);
+ //student.trigger('study');
+ //student.trigger('hmmm... exception?');
+ //console.log(ev + ' ' + this.config.states[this.state].transitions[ev] );
+ //student.trigger();
+ //student.changeState('busy');
+ //student.trigger();
 
 module.exports = FSM;
 
